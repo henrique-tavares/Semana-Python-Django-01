e96368e1cb2649c8825163d5a4be9edbed5a5776
henrique-tavares <ricktavares76@gmail.com>
2020-06-24 16:35:38 -0300

    adicionando usuário

--- gerenciador_tarefas/app/urls.py
+++ gerenciador_tarefas/app/urls.py
@@ -1,7 +1,19 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
-from .views import *
+from .views.tarefa_views import (
+    listar_tarefas,
+    cadastrar_tarefa,
+    editar_tarefa,
+    remover_tarefa,
+)
+from .views.usuario_views import cadastrar_usuario, logar_usuario, deslogar_usuario
 
 urlpatterns = [
-    path('listar_tarefas/', listar_tarefas, name='listar_tarefas'),
+    path("listar_tarefas/", listar_tarefas, name="listar_tarefas"),
+    path("cadastrar_tarefa/", cadastrar_tarefa, name="cadastrar_tarefa"),
+    path("editar_tarefa/<int:id>", editar_tarefa, name="editar_tarefa"),
+    path("remover_tarefa/<int:id>", remover_tarefa, name="remover_tarefa"),
+    path("cadastrar_usuario/", cadastrar_usuario, name="cadastrar_usuario"),
+    path("logar_usuario/", logar_usuario, name="logar_usuario"),
+    path("deslogar_usuario/", deslogar_usuario, name="deslogar_usuario"),
 ]
