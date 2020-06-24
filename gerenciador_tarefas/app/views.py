a9ea2751004ea860e650be7967ec09885893cab5
henrique-tavares <ricktavares76@gmail.com>
2020-06-24 16:31:24 -0300

    organizando e adicionando views

--- gerenciador_tarefas/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def listar_tarefas(request):
-    nome_tarefa = "Assistir a Semana Python e Django da Treinaweb"
-
-    return render(request, 'tarefas/listar_tarefas.html', {"nome_tarefa": nome_tarefa})
